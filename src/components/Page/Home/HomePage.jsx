b0a37eaf82be4e90c42207871bb15dd7ce8083fa
Anuj Jha <anujjha710@gmail.com>
2023-03-30 01:48:29 +0530

    Error Fixed of Home Page

--- src/components/Page/Home/HomePage.jsx
+++ src/components/Page/Home/HomePage.jsx
@@ -1,19 +1,17 @@
 import React, { useEffect, useState } from "react"
-import "./HomePage.css"
+import "../Home/HomePage.css"
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import { Link } from "react-router-dom";
-import MovieList from '../MovieDetail/MovieList'
-
 
 const HomePage = () => {
 
-    const [popularMovies, setPopularMovies] = useState([])
+    const [ popularMovies, setPopularMovies ] = useState([])
 
     useEffect(() => {
         fetch("https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US")
-            .then(res => res.json())
-            .then(data => setPopularMovies(data.results))
+        .then(res => res.json())
+        .then(data => setPopularMovies(data.results))
     }, [])
 
     return (
@@ -28,16 +26,16 @@ const HomePage = () => {
                 >
                     {
                         popularMovies.map(movie => (
-                            <Link style={{ textDecoration: "none", color: "white" }} to={`/movie/${movie.id}`} >
+                            <Link style={{textDecoration:"none",color:"white"}} to={`/movie/${movie.id}`} >
                                 <div className="posterImage">
                                     <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} alt='img' />
                                 </div>
                                 <div className="posterImage__overlay">
-                                    <div className="posterImage__title">{movie ? movie.original_title : ""}</div>
+                                    <div className="posterImage__title">{movie ? movie.original_title: ""}</div>
                                     <div className="posterImage__runtime">
                                         {movie ? movie.release_date : ""}
                                         <span className="posterImage__rating">
-                                            {movie ? movie.vote_average : ""}
+                                            {movie ? movie.vote_average :""}
                                             <i className="fas fa-star" />{" "}
                                         </span>
                                     </div>
@@ -47,11 +45,9 @@ const HomePage = () => {
                         ))
                     }
                 </Carousel>
-                <MovieList />
             </div>
         </>
     )
 }
-
 
 export default HomePage
